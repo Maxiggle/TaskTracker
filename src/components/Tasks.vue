d04bcb0174926d92081a8c85d1c6ec68149cde32
Godwin Alexander Ekainu <clsfriday@gmail.com>
2021-08-29 14:21:46 +0100

    first commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="index in tasksArray" :key="index.id" >
+    <Task @toggle-reminder="$emit('toggle-reminder', index.id)" @delete-task="$emit('delete-task',index.id)" :task="index" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+  name: "Tasks",
+  props: {
+    tasksArray: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task','toggle-reminder'],
+};
+</script>
+
---
+++ src/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('shownewTask')"
+      :text="showbutton ? 'Close' : 'add task'"
+      :color="showbutton ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showbutton: Boolean,
+  },
+  components: {
+    Button,
+  },
+  // computed: {
+  //   homePage() {
+  //     if (this.$route.path === '/') {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   },
+  // },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
